29ae6ac737637a6a6e923fbb291a1d2ce93b8f90
AlanSub <alansubwork@gmail.com>
2024-05-06 18:43:11 +0800

    init commit

---
+++ Step2_build/MathFunctions/CMakeFiles/MathFunction.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathFunction.dir/MathFunctions.cxx.o"
+  "CMakeFiles/MathFunction.dir/MathFunctions.cxx.o.d"
+  "CMakeFiles/MathFunction.dir/mysqrt.cxx.o"
+  "CMakeFiles/MathFunction.dir/mysqrt.cxx.o.d"
+  "libMathFunction.a"
+  "libMathFunction.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathFunction.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
